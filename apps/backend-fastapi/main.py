4ef66a2d0d304c3ef9c3a70e7e8be116cf35b274
KSS Hettiarachchi <kavindukv@live.com>
2025-04-20 22:30:27 +1000

    minor change to cors middleware

--- apps/backend-fastapi/main.py
+++ apps/backend-fastapi/main.py
@@ -5,12 +5,4 @@ from api.router import router
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(router)
